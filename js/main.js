afeb1563d56cd7dcf03a1f7c0967928f9a15467c
ErikBorgogno <erikborgo.20@gmail.com>
2022-08-22 17:27:26 +0200

    done project

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 const email = new Vue({
-    el: '#email',
-    data: {
+    el:'#email',
+    data:{
         emails: []
     },
     mounted(){
         for(let i = 0; i < 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(response => {
+             .then(response =>{
                 this.emails.push(response.data.response);
-            })
+             })
         }
         console.log(this.emails)
-        
-    },
-});         
+    }
+});     
